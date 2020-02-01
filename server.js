a2bd4c1fb654a224a5344615dab7ba50ffb811c1
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2020-02-01 21:57:59 +0100

    add handlebars

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use((req, res, next) => {
   res.show = name => {
@@ -13,7 +17,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/hello/:name', (req, res) => {
-  res.send(`Hello ${req.params.name}`);
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.get('/', (req, res) => {
